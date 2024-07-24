a0e26ad369f0f0b47953d6b441c8d9220c5bd8ad
Francisco Lacerda <flacerda@vasp.pt>
2024-07-24 19:54:09 +0100

    RemoveDuplicatesFromSortedArrayTests

---
+++ CodeKatas.Solution/CodeKatasTests/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+
+namespace CodeKatasTests
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Should_RemoveDuplicatesFromSortedArray_ReturnNumberUniqueElements()
+        {
+            //Arrange
+            var numbers = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+            //Act
+
+            var result = RemoveDuplicatesFromSortedArray(numbers);
+
+            //Assert
+            result.Should().Be(5);
+        }
+
+        private int RemoveDuplicatesFromSortedArray(int[] numbers)
+        {
+            var j = 0;
+            var dic = new HashSet<int>();
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (!dic.Contains(numbers[i]))
+                {
+                    dic.Add(numbers[i]);
+                    numbers[j] = numbers[i];
+                    j++;
+                }
+            }
+
+            return j;
+        }
+    }
+}
